57b3ed8f838a897709da1e2a45683873b6932374
diaokaizi <1433137915@qq.com>
2024-02-01 17:59:01 +0800

    过滤infterface

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+from mysql import MySQL
+import datetime
 import requests
 import json
 import datetime
 from urllib import parse
+
+
 class netflowObj:
     def __init__(self, src_ip, des_ip, packets, bytes, proto):
         self.src_ip = src_ip
@@ -35,6 +39,7 @@ def lokiapi(host: str, start: datetime.datetime, end: datetime.datetime, limit: 
     return parse_lokiapi_data(resp.text)
 
 def parse_lokiapi_data(data : str) -> list[netflowObj] :
+    f1, f2 = 0, 0
     try:
         result = json.loads(data)['data']['result']
         if len(result) == 0: #数据为空
@@ -46,11 +51,45 @@ def parse_lokiapi_data(data : str) -> list[netflowObj] :
     data = []
     for newflow in newflows:
         try:
-            ## 添加过滤条件egressInterface==35
-            if newflow['egressInterface'] != 35:
+            # 添加过滤条件egressInterface==35
+            if newflow['flowDirection'] != 1:
+                f1 += newflow['network']['bytes']
                 continue
+            else:
+                f2 += newflow['network']['bytes']
             data.append(netflowObj(src_ip=newflow['source']['ip'], des_ip=newflow['destination']['ip'], packets=newflow['network']['packets'],
                     bytes=newflow['network']['bytes'], proto=newflow['network']['transport']))
-        except:
-            print("parse_netflow_obj_error", data)
-    return data
+        except Exception as e:
+            print("parse_netflow_obj_error", data, e)
+    return data, f1, f2
+
+def get_latest_5m_start_datetime():
+    now = datetime.datetime.now()
+    timestamp = datetime.datetime.timestamp(now)
+    return datetime.datetime.fromtimestamp(timestamp - timestamp % 300 - 300)
+host="223.193.36.79:7135" # netflow loki地址
+ip = "202.127.3.156"
+start = get_latest_5m_start_datetime()
+data = []
+data, f1, f2 = lokiapi(host=host, start = start, end = start + datetime.timedelta(minutes=5), limit=500000, search=ip)
+
+print(f1, f2)
+dic = {}
+total1, total2 = 0, 0
+for _ in data:
+    dic[str(_)] = dic.get(str(_), 0) + 1
+    total1 += _.bytes
+print(total1)
+
+# print(total1)
+
+# total, p = 0, 0
+# for _ in data:
+#     total += _.bytes
+#     if dic[str(_)] > 1:
+#         p += _.bytes
+
+# print(total, p)
+
+
+	
